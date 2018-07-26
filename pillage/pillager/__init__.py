77d7c5baeac994bc36078271f501e33d5fab801c
Guy Royse <guy@guyroyse.com>
2018-07-26 11:08:51 -0400

    refactored init for flask

--- pillage/pillager/__init__.py
+++ pillage/pillager/__init__.py
@@ -2,32 +2,31 @@ import os
 
 from flask import Flask, send_from_directory
 
-
 def create_app(test_config=None):
-  # create and configure the app
   app = Flask(__name__, instance_relative_config=True)
+  config_app(app, test_config)
+  create_instance_folder(app)
+  setup_static_routes(app)
+  return app
+
+def config_app(app, test_config):
   app.config.from_mapping(
     SECRET_KEY='dev',
     DATABASE=os.path.join(app.instance_path, 'pillager.sqlite')
   )
 
   if test_config is None:
-    # load the instance config, if it exists, when not testing
     app.config.from_pyfile('config.py', silent=True)
   else:
-    # load the test config if passed in
     app.config.from_mapping(test_config)
 
-  # ensure the instance folder exists
+def create_instance_folder(app):
   try:
     os.makedirs(app.instance_path)
   except OSError:
     pass
 
-  @app.route('/hello')
-  def hello():
-    return 'Hello, World!'
-
+def setup_static_routes(app):
   @app.route('/')
   def index():
     return send_from_directory('static', 'index.html')
@@ -36,4 +35,3 @@ def create_app(test_config=None):
   def send_static(path):
     return send_from_directory('static', path)
 
-  return app
