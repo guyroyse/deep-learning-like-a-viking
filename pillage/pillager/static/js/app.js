7575d8c6ece7cf317118071f0177e77bd95bf755
Guy Royse <guy@guyroyse.com>
2018-07-26 17:45:45 -0400

    added css and js to draw images

---
+++ pillage/pillager/static/js/app.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  let canvas = document.getElementById('canvas');
+  let otherCanvas = document.getElementById('otherCanvas');
+
+  let save = document.getElementById('save');
+
+  let context = canvas.getContext('2d');
+
+  let otherContext = otherCanvas.getContext('2d');
+  otherContext.scale(1, 1);
+
+  context.lineWidth = 20;
+  context.lineCap = 'round';
+
+  let prevX = null;
+  let prevY = null;
+  let drawing = false;
+
+  canvas.addEventListener('mousedown', function(event) {
+    drawing = true;
+    let currentX = event.clientX - canvas.offsetLeft - 1;
+    let currentY = event.clientY - canvas.offsetTop - 1;
+
+    context.beginPath();
+    context.moveTo(currentX, currentY);
+
+    prevX = event.clientX - canvas.offsetLeft - 1;
+    prevY = event.clientY - canvas.offsetTop - 1;
+  });
+
+  canvas.addEventListener('mousemove', function(event) {
+
+    if (drawing) {
+      let currentX = event.clientX - canvas.offsetLeft - 1;
+      let currentY = event.clientY - canvas.offsetTop - 1;
+
+      context.lineTo(currentX, currentY);
+      context.stroke();
+
+      prevX = currentX;
+      prevY = currentY;
+    }
+
+  });
+
+  canvas.addEventListener('mouseup', function(event) {
+    let currentX = event.clientX - canvas.offsetLeft - 1;
+    let currentY = event.clientY - canvas.offsetTop - 1;
+
+    context.lineTo(currentX, currentY);
+    context.stroke();
+
+    drawing = false;
+    prevX = null;
+    prevY = null;
+  });
+
+  save.addEventListener('click', function() {
+    let imageData = context.getImageData(0, 0, 240, 240);
+    console.log(imageData);
+
+    createImageBitmap(imageData).then(function(imageBitmap) {
+      console.log("promise resolved");
+      otherContext.scale(0.1, 0.1);
+      otherContext.drawImage(imageBitmap, 0, 0);
+    });
+
+  });
+
+});
