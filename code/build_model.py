ca8e77260ca4e3be50ee02cd421c14dc7dfd8cb2
Guy Royse <guy@guyroyse.com>
2018-08-27 14:55:15 -0400

    extracted code for futhark model

--- code/build_model.py
+++ code/build_model.py
@@ -17,6 +17,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 from common.rune_file import RuneFile
+from common.futhark_model import FutharkModel
 
 X, y = [], []
 
@@ -32,38 +33,8 @@ label_encoder = LabelEncoder()
 
 y = label_encoder.fit_transform(y)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
-
-Y_train = np_utils.to_categorical(y_train, 16)
-Y_test = np_utils.to_categorical(y_test, 16)
-
-model = Sequential()
-model.add(Conv2D(48, (3, 3), activation='relu', input_shape=(1, 24, 24)))
-model.add(Conv2D(48, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(16, activation='softmax'))
-
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-model.fit(X_train, Y_train, batch_size=32, epochs=10, verbose=1)
-
-score = model.evaluate(X_test, Y_test, verbose=0)
+model = FutharkModel()
+score = model.build(X, y)
+model.save()
 
 print(score)
-
-# serialize model to JSON
-model_json = model.to_json()
-with open("futhark_model.json", "w") as json_file:
-  json_file.write(model_json)
-
-# serialize weights to HDF5
-model.save_weights("futhark_model.h5")
---
+++ code/common/futhark_model.py
@@ -0,0 +1,55 @@
+import os
+os.environ['KERAS_BACKEND'] = 'theano'
+
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+from keras.utils import np_utils
+from keras import backend as K
+K.set_image_dim_ordering('th')
+
+from sklearn.model_selection import train_test_split
+
+class FutharkModel:
+
+  def build(self, X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+    X_train /= 255
+    X_test /= 255
+
+    Y_train = np_utils.to_categorical(y_train, 16)
+    Y_test = np_utils.to_categorical(y_test, 16)
+
+    model = Sequential()
+    model.add(Conv2D(48, (3, 3), activation='relu', input_shape=(1, 24, 24)))
+    model.add(Conv2D(48, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(16, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    model.fit(X_train, Y_train, batch_size=32, epochs=10, verbose=1)
+
+    self.__model = model
+
+    return model.evaluate(X_test, Y_test, verbose=0)
+
+  def save(self):
+    model_json = self.__model.to_json()
+    with open("model/futhark_model.json", "w") as json_file:
+      json_file.write(model_json)
+    self.__model.save_weights("model/futhark_model.h5")
+
+  def load(self):
+    return 0
+
+  def predict(self):
+    return 0
