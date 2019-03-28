31b05752eb81aae43e4c38e8743ed4deb5bae5d2
Guy Royse <guy@guyroyse.com>
2019-03-28 16:38:57 -0400

    a more readable bit of sample code

--- code/app.py
+++ code/app.py
@@ -1,48 +1,122 @@
 import os
-import functools
+import json
+
+import numpy as np
+import tensorflow as tf
 
 from flask import Flask, send_from_directory, request, jsonify
 
+from keras.models import model_from_json
+from sklearn.preprocessing import LabelEncoder
+
 from uuid import uuid4
 
-from rune_data import RuneData
-from futhark_model import FutharkModel
-from futhark_labels import FutharkLabels
-
+# the Flask app
 app = Flask(__name__, instance_relative_config=True)
 
+# model and graph need to be be global when working with TensorFlow
+global model, graph
+
+# load the model structure and weights for the saved off files
+with open('model/futhark_model.json', 'r') as file:
+  model_json = file.read()
+  file.close()
+
+model = model_from_json(model_json)
+model.load_weights('model/futhark_model.h5')
+
+# get the graph so we can be sure to use it in the future
+graph = tf.get_default_graph()
+
+# setup the label encoder
+encoder = LabelEncoder()
+encoder.fit([
+  "fe", "ur", "thurs", "as", "reith", "kaun", "hagall", "nauthr",
+  "isa", "ar", "sol", "tyr", "bjork", "mathr", "logr", "yr"
+])
+
+
+
+# saving runes
 @app.route('/rune/save', methods=['POST'])
 def save_rune():
+
+  # get the JSON containing the name and image of the rune
+  rune_json = request.get_json()
+
+  # generate a random filename
   filename = f'{str(uuid4())}.json'
 
-  rune = RuneData.from_json(request.get_json())
-  rune.save(f'data/{filename}')
+  # write the contents of the rune out to the file
+  with open(f'data/{filename}', 'w') as file:
+    file.write(json.dumps(rune_json))
+    file.close
 
-  print(f"Saving rune '{rune.rune_name}' to file {filename}")
+  # tell everyone about it
+  print(f"Saving rune '{rune_json['rune']}' to file {filename}")
 
+  # return a simple OK message
   return jsonify(message="OK")
 
+
+
+# detecting runes
 @app.route('/rune/detect', methods=['POST'])
 def detect_rune():
-  labels = FutharkLabels()
-  model = FutharkModel()
-  model.load()
 
-  rune = RuneData.from_json(request.get_json())
-  encoded_rune = model.predict_one(rune.image_data)
-  rune_label = labels.decode_one(encoded_rune)
+  # get the JSON containing the image of the rune
+  rune_json = request.get_json()
 
-  print(f"Detected rune of '{encoded_rune}' decodes to '{rune_label}'")
+  # print out the data for the image in a human friendly fashion
+  for row in rune_json['data']:
+    print()
+    for cell in row:
+      print("{:^4d}".format(cell), end='')
 
-  return jsonify(message="OK", rune=rune_label)
+  print()
 
+  # convert the image to numpy for use by the model
+  #
+  # NOTE: We are adding two more dimensions to the image. The first
+  # provides depth for RGB images which Keras expects. We are doing
+  # the alpha channel only so we only have one dimension there. The
+  # second is that Keras expects to classify an array of images. We
+  # only want to classify one image. So, just one there as well.
+  rune_image = np.array(rune_json['data']).reshape(1, 1, 24, 24)
+
+  # tell TensorFlow which graph to use and then make a prediction
+  #
+  # NOTE: This returns and array of predictions with only one
+  # prediction in it.
+  with graph.as_default():
+    runes = model.predict_classes(rune_image)
+
+  # decoded the predictions from a numeric labels to strings
+  rune_labels = encoder.inverse_transform(runes)
+
+  # tell everyone about it
+  print(f"Detected rune of '{runes}' decodes to '{rune_labels}'")
+  print()
+
+  # return just the first prediction
+  return jsonify(message="OK", rune=rune_labels[0])
+
+
+
+# this route serves up 'index.html'
 @app.route('/')
 def index():
   return send_from_directory('static', 'index.html')
 
+
+
+# this route serves up and other paths to the static folder
 @app.route('/<path:path>')
 def send_static(path):
   return send_from_directory('static', path)
 
+
+
+# kick of the Flask application
 if __name__ == '__main__':
   app.run()
--- code/build_model.py
+++ code/build_model.py
@@ -1,38 +1,88 @@
 import os
+import json
 import numpy as np
 
-from rune_data import RuneData
-from futhark_model import FutharkModel
-from futhark_labels import FutharkLabels
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
-model = FutharkModel()
-labels = FutharkLabels()
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+from keras.utils import np_utils
 
+# X holds the images, y holds the labels
 X, y = [], []
 
 print()
 print("Loading runic JSON files...")
 
+# loop over all the data files
 for filename in os.listdir('data'):
-  rune = RuneData.from_file(f'data/{filename}')
-  X.append(rune.image_data)
-  y.append(rune.rune_name)
-  print(f"  ...'{rune.rune_name}' from {filename}")
+  with open(f'data/{filename}') as file:
 
-X = np.array(X)
-y = np.array(y)
+    # parse the JSON within
+    json_data = json.load(file)
+    file.close
 
-y = labels.encode(y)
+    # extract the image and the label from the JSON
+    rune_image = np.array(json_data['data']).reshape(1, 24, 24)
+    rune_name = json_data['rune']
+
+    # add the image and the label to their respective arrays
+    X.append(rune_image)
+    y.append(rune_name)
+
+    print(f"  ...'{rune_name}' from {filename}")
 
 print()
-print("Building model...")
 
-score = model.build(X, y)
-model.save()
+# convert X and y to numpy arrays
+X, y = np.array(X), np.array(y)
 
+# scale the image data from 0...255 to 0.0...1.0
+X = X.astype('float32')
+X /= 255
+
+# create an encoder for the labels
+encoder = LabelEncoder()
+encoder.fit([
+  "fe", "ur", "thurs", "as", "reith", "kaun", "hagall", "nauthr",
+  "isa", "ar", "sol", "tyr", "bjork", "mathr", "logr", "yr"
+])
+
+# convert the labels from strings to numbers
+y = encoder.transform(y)
+
+# convert the labels from numbers to one-hot encoding
+Y = np_utils.to_categorical(y, 16)
+
+# split for train and test
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+# configure the neural network
+model = Sequential()
+model.add(Conv2D(48, (3, 3), activation='relu', data_format='channels_first', input_shape=(1, 24, 24)))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(24, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(16, activation='softmax'))
+
+# compile the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# train it
+model.fit(X_train, Y_train, batch_size=32, epochs=20, verbose=1)
+
+# evaluate it
 print()
-print("Modeled saved")
+print(model.evaluate(X_test, Y_test, verbose=0))
 print()
 
-print("Model Score:", score)
+# save it
+model.save_weights('model/futhark_model.h5')
+with open('model/futhark_model.json', 'w') as file:
+  file.write(model.to_json())
+
+print("Saved model to 'model/futhark_model.json' and 'model/futhark_model.h5'")
 print()
