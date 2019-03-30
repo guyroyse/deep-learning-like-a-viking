fe7561bf271a1533a60b5a95c0a48f32ab82a975
Guy Royse <guy@guyroyse.com>
2019-03-30 09:17:02 -0400

    line breaks to make code fit on screen for demo

--- code/build_model.py
+++ code/build_model.py
@@ -56,11 +56,13 @@ y = encoder.transform(y)
 Y = np_utils.to_categorical(y, 16)
 
 # split for train and test
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y,
+  test_size=0.2, random_state=42)
 
 # configure the neural network
 model = Sequential()
-model.add(Conv2D(48, (3, 3), activation='relu', data_format='channels_first', input_shape=(1, 24, 24)))
+model.add(Conv2D(48, (3, 3), activation='relu',
+  data_format='channels_first', input_shape=(1, 24, 24)))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Conv2D(24, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
@@ -69,7 +71,8 @@ model.add(Dense(128, activation='relu'))
 model.add(Dense(16, activation='softmax'))
 
 # compile the model
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy',
+  optimizer='adam', metrics=['accuracy'])
 
 # train it
 model.fit(X_train, Y_train, batch_size=32, epochs=20, verbose=1)
@@ -84,5 +87,7 @@ model.save_weights('model/futhark_model.h5')
 with open('model/futhark_model.json', 'w') as file:
   file.write(model.to_json())
 
-print("Saved model to 'model/futhark_model.json' and 'model/futhark_model.h5'")
+print("Saved model to:")
+print("  model/futhark_model.json")
+print("  model/futhark_model.h5")
 print()
